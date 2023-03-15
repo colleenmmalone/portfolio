3b6b86b90a62961491436ededb73f7385d9a27b8
colleencxstudios <colleen@cxstudios.io>
2023-03-15 00:50:29 -0400

    overflow scrolling, fixed error page redirect issue

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,10 +14,10 @@ const Home = () => {
             <div className="my-auto">
               <h1 className="text-[82px] "> Hi! I’m Colleen.</h1>
               <h3 className="text-[62px] leading-[75px] my-7" >I identify as a full-stack web developer, but I am learning UX Visual Design in Figma.</h3>
-              <div className="flex mx-auto mt-12 w-fit">
+              <div className="flex mx-auto mt-12 w-fit flex-wrap">
                 {imgs.map((i) => (
-                  <div className="h-[60px] min-w-[60px] bg-[#eee] mx-3 p-1 rounded flex">
-                    <img src={i} className="h-full mx-auto" />
+                  <div className="h-[60px] min-w-[60px] bg-[#eee] m-3 p-1 rounded flex" >
+                    <img src={i} className={` h-full m-auto`} />
                   </div>
                 )
                 )}
@@ -26,7 +26,7 @@ const Home = () => {
             </div>
           </div>
 
-          <div className="col-span-4 bg-white p-default h-full">
+          <div className="div-scroll col-span-4 bg-white p-default">
             <div className="w-3/4 h-[400px] bg-[#ccc] mx-auto my-5"></div>
             <div className="w-3/4 h-[400px] bg-[#ccc] mx-auto my-5"></div>
             <div className="w-3/4 h-[400px] bg-[#ccc] mx-auto my-5"></div>
